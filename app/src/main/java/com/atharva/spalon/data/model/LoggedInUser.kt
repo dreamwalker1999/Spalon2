bc771c2222cfea4684fcdffcea43110874b40098
atharva <atharva.lord10@gmail.com>
2019-06-01 10:02:11 +0530

    Initial commit

---
+++ app/src/main/java/com/atharva/spalon/data/model/LoggedInUser.kt
@@ -0,0 +1,9 @@
+package com.atharva.spalon.data.model
+
+/**
+ * Data class that captures user information for logged in users retrieved from LoginRepository
+ */
+data class LoggedInUser(
+    val userId: String,
+    val displayName: String
+)
